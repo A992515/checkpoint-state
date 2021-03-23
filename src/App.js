3b8e2b5654068e5afe927f9cedfdb5aa32b1600e
Ahlem Ahmed <ahmedahlem3@gmail.com>
2021-03-23 15:01:01 +0100

    final modification

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import React, { Component } from 'react'
+import icone from "./icone.png";
 export default class App extends Component {
   constructor() {
     super()
@@ -10,29 +11,30 @@ export default class App extends Component {
       bio : "I’m curious, and I enjoy work that challenges me to learn something new and stretch in a different direction. I do my best to stay on top of changes in the state of the art so that I can meet challenges with tools well suited to the job at hand.",  
       profession: "I’m a web devoloper", 
       showHideProfile: false,
-      counter: 0
+      timer: 0
     };
   }
-  
-  componentDidMount(){
-      this.myInterval = setInterval(()=>{
-        this.setState({counter: this.state.counter +1}) 
-      },1000)
-    }
-    componentWillUnmount() {
-      clearInterval(this.myInterval);
-    }
     hideComponent() {
+      if (this.state.showHideProfile== true){this.setState({timer:0})}
+      else{this.setState({timer: 0})}
       this.setState({ showHideProfile: !this.state.showHideProfile
       });
-      this.setState({counter: this.state.counter +1});
 }
+componentDidMount(){
+  this.myInterval = setInterval(()=>{
+    this.setState({timer: this.state.timer +1}) 
+  },1000)
+}
+// componentWillUnmount() {
+//   clearInterval(this.myInterval) 
+//   console.log(clearInterval)
+// }
   render() {
     return (
       <div className="App">
-        {
-          this.state.showHideProfile?
-          <div>
+        {this.state.showHideProfile?
+        <div className="container">
+        <img src={icone} alt="profile-picture" width={100}/>
         <h1>{this.state.fullName}</h1>
         <h5>{this.state.bio}</h5>
         <h4>{this.state.imgSrc}</h4>
@@ -40,11 +42,9 @@ export default class App extends Component {
          </div>
          :null
         }
-        <button onClick={() => this.hideComponent()}>
+        <button onClick={() =>{this.hideComponent()} }>
        show Profile 
      </button>
-     <h4>{this.state.counter}</h4>
+     <h4>{this.state.showHideProfile ? this.state.timer:[]}</h4>
      </div>
-    )
-  }
-}
+    )}}
